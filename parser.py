24a98ccd9a1236ab6d5af6a1fb523712e14e3e89
feffemannen <fredrik.ellborg@gmail.com>
2021-12-15 21:17:41 +0100

    Made changes to parser to take input from file 'abox.txt' and cleaned up the code.

--- parser.py
+++ parser.py
@@ -1,37 +1,61 @@
-# A simple parser that takes formulas to be included in the ABox and *prints* all the actr commands that should be included. 
-# Note:
-# - Not sure if negation is treated as it should (as you do). 
-# - It should be easy to change "print" to actually add the chunks to the dm
-# - It should also be easy to expand for existential restriction, etc.
-# - And to add the abox input as a file.
-#
-# Obs. This is my first try at coding python. Change and fix as you like.
+from lark import Tree,Token,Lark, Transformer,Visitor, v_args
+import pyactr as actr
 
-from lark import Tree,Token,Lark, Transformer, v_args
+aBoxCon = actr.ACTRModel(
+    automatic_visual_search=False,
+    motor_prepared=True,
+ #   subsymbolic=True,
+    utility_noise=0.2,
+    partial_matching=False,
+ #   utility_learning=True,
+ #   production_compilation=True,
+    activation_trace=True,
+    retrieval_threshold=0,
+    decay=0
+)
 
-calc_grammar = """
-    ?start: element ":" out_concept   -> con_ass
-        |   "(" element "," element ")" ":" role -> role_ass
+aBoxCon.goals = {}
+aBoxCon.set_goal("g")
+aBoxCon.set_goal("imaginal")
+aBoxCon.set_goal("imaginal_action")
+
+actr.chunktype("goal", "state")
+actr.chunktype("proposition", "thing, form, element, mainconnective, relation, subformula1, inferred1, subformula2, inferred2, derived")
+actr.chunktype("checklist", "thing, form, element, mainconnective, relation, subformula1, subformula2, form2, form3, form4, form5, form6, form7, form8")
+actr.chunktype("storelist", "thing, form, element, mainconnective, relation, subformula1, subformula2, derived, inferred1, inferred2, form2, form3, form4, form5, form6, form7, form8, form9, form10, form11, form12, form13, form14, form15")
+
+dm = aBoxCon.decmem
+goal = aBoxCon.goals["g"]
+imaginal = aBoxCon.goals["imaginal"]
+imaginal_action = aBoxCon.goals["imaginal_action"]
+retrieval = aBoxCon.retrieval
+production = aBoxCon.productions
+
+
+form_grammar = """
+
+    ?start: assertion (","+ assertion)*
+
+    ?assertion: element ":" out_concept             -> con_ass
+        | "(" element "," element ")" ":" role  -> role_ass
 
     ?out_concept: atom                            
-        | "-" atom              -> not
-        | concept "&" concept   -> con
-        | "(" concept "&" concept ")" -> con
-        | concept "%" concept   -> dis
-        | "(" concept "%" concept ")"   -> dis
-        | "/E" role "." concept -> exists
-        | "/A" role "." concept -> forall
+        | "-" atom                          -> neg
+        | ["("] concept "&" concept [")"]   -> conj
+        | ["("] concept "%" concept [")"]   -> dis
+        | "/E" role "." concept             -> exists
+        | "/A" role "." concept             -> forall
 
     ?concept: atom                            
-        | "-" atom              -> not
-        | "(" concept "&" concept ")"   -> con
+        | "-" atom                      -> neg
+        | "(" concept "&" concept ")"   -> conj
         | "(" concept "%" concept ")"   -> dis
-        | "/E" role "." concept -> exists
-        | "/A" role "." concept -> forall
+        | "/E" role "." concept         -> exists
+        | "/A" role "." concept         -> forall
 
-    ?role: NAME                 -> role
-    ?element : NAME             -> element
-    ?atom : NAME                -> atom
+    ?role: NAME                         -> role
+    ?element : NAME                     -> element
+    ?atom : NAME                        -> atom
 
     %import common.CNAME -> NAME
     %import common.WS_INLINE
@@ -39,47 +63,64 @@ calc_grammar = """
     %ignore WS_INLINE
 """
 
-form_parser = Lark(calc_grammar, parser='lalr')
+@v_args(inline=True)    # Affects the signatures of the methods
+class ToString(Transformer): # Transforms a tree recursively to a string
+    role = str 
+    element = str 
+    atom = str 
+    conj = lambda self,ch1,ch2: "(" + ch1 + "&" + ch2 + ")"
+    con_ass = lambda self,el,con: el + ":" + con
+    neg = lambda self,con : "-" + con
+
+class AddFormToAbox(Visitor): # Adds a formula together with all subformulas to the DM. 
+
+    def __init__(self):
+        self.el=""
+        self.derived="yes"
+
+    def con_ass(self,tree):
+        self.el=tree.children[0].children[0]
+
+    def conj(self,tree):
+        formstr = self.el + ":" + ToString().transform(tree)
+        subformL = self.el + ":" + ToString().transform(tree.children[0])
+        subformR = self.el + ":" + ToString().transform(tree.children[1])
+        dm.add(actr.makechunk(typename="proposition", thing="proposition", 
+            form=formstr, element=self.el, mainconnective="conjunction", 
+            subformula1=subformL, subformula2=subformR, 
+            derived=self.derived, inferred1="no", inferred2="no"))
+        self.derived="no"
+
+    def neg(self,tree):
+        formstr = self.el + ":" + ToString().transform(tree)
+        subform = ToString().transform(tree.children[0])
+        dm.add(actr.makechunk(typename="proposition", thing="proposition", 
+            form=formstr, element=self.el, mainconnective="negation", 
+            subformula1=subform, derived=self.derived, inferred1="no"))
+        self.derived="no"
+
+    def atom(self,tree):
+        formstr = self.el + ":" + ToString().transform(tree)
+        subform = ToString().transform(tree)
+        dm.add(actr.makechunk(typename="proposition", thing="proposition", 
+            form=formstr, element=self.el, mainconnective="concept", 
+            subformula1=subform, derived=self.derived, inferred1="no"))
+        self.derived="no"
+
+form_parser = Lark(form_grammar, parser='lalr')
 parser = form_parser.parse
 
-def TreeToString(form):
-    if form.data == "con_ass":
-        return(form.children[0].children[0] + ":" + TreeToString(form.children[1]))
-    elif form.data == "con":
-        return("(" + TreeToString(form.children[0]) + "&" + TreeToString(form.children[1])+ ")")
-    elif form.data == "not":
-        return("-" + TreeToString(form.children[0]))
-    elif form.data == "atom":
-        return(form.children[0])
+## Test of reading ABoxes from file:
 
-def create_form_chunk(form,derived):
-    if form.data == "con_ass":
-        el=form.children[0].children[0]
-        formtype=form.children[1].data
-        subs=form.children[1].children
-        if formtype == "atom":
-            print('dm.add(actr.makechunk(typename="proposition", thing="proposition", form="'+TreeToString(form)+'", element="'+el+'", mainconnective="concept", subformula1="'+el+':'+subs[0]+'", derived="'+derived+'")')
-        elif formtype == "not":
-            print('dm.add(actr.makechunk(typename="proposition", thing="proposition", form="'+TreeToString(form)+'", element="'+el+'", mainconnective="negation", subformula1="'+el+':'+TreeToString(subs[0])+'", derived="'+derived+'", inferred1="no"))')        
-        elif formtype == "con":
-            print('dm.add(actr.makechunk(typename="proposition", thing="proposition", form="'+TreeToString(form)+'", element="'+el+'", mainconnective="conjunction", subformula1="'+el+':'+TreeToString(subs[0])+'", subformula2="'+el+':'+TreeToString(subs[1])+'", derived="'+derived+'", inferred1="no", inferred2="no"))')
+with open('abox.txt', 'r') as file:
+    data = file.read().replace('\n', ',') 
+abox = parser(data)
+if abox.data == "con_ass":
+    aboxlst = [abox]
+else:
+    aboxlst=abox.children
+for form in aboxlst:
+    AddFormToAbox().visit_topdown(form) # Need to visit the tree top down to get the element first.
+    print(ToString().transform(form))
 
-def con_ass(el,concept):
-    return(Tree("con_ass",[Tree('element', [Token('NAME', el)]),concept]))
-
-def add_form_to_abox(form,derived="yes"):
-    create_form_chunk(form,derived)
-    if form.data == "con_ass":
-        el=form.children[0].children[0]
-        formtype=form.children[1].data
-        subs=form.children[1].children
-        if formtype == "not":
-            create_form_chunk(con_ass(el,subs[0]),"no")
-        elif formtype == "con":
-            add_form_to_abox(con_ass(el,subs[0]),"no")
-            add_form_to_abox(con_ass(el,subs[1]),"no")
-
-for x in ["a : c", "a:-c", "b:-d&a", "b:-d & ((a&c) &b)"]:
-    form=parser(x)
-    print(TreeToString(form))
-    add_form_to_abox(form)
+print(dm)
