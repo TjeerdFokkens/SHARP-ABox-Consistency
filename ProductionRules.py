92e9a4f98413e0c3c3f1b21c8a03887ff25f058b
TjeerdFokkens <tjeerdfokkens@gmail.com>
2022-01-13 17:31:42 +0100

    Everything ready to be tested
    Parser updated to fit new way of doing things. Module 4 finished. Minor aesthetic changes. Tomorrow is testing day. Fingers crossed.

--- ProductionRules.py
+++ ProductionRules.py
@@ -3,10 +3,10 @@
 #When several different production rules can fire after given one, they form new units, each labelled by a, b, etc.
 
 def module1(aBoxCon): #This module looks for concept assignments and negated concept assignments and tries to find a clash. If there is no clash, some new formula needs to be derived.
-    aBoxCon.productionstring(name="Module 1, Unit 1: find first concept or negation, Step 1", string="""
+    aBoxCon.productionstring(name="Module 1, Unit 1, Step 1: find clash to concept or negation in head of list", string="""
         =g>
         isa     goal
-        state   find_clash
+        state   find_clash_to_head
         form    =P
         =imaginal>
         isa     checklist
@@ -29,8 +29,24 @@ def module1(aBoxCon): #This module looks for concept assignments and negated con
         ==>
         =g>
         isa     goal
-        state   next_concept_negation
+        state   find_formula_not_in_list
         form    =P
+        =imaginal>
+        isa     checklist
+        thing   checklist
+        form    =A1
+        element =A2
+        mainconnective =A3
+        relation =A4
+        subformula1 =A5
+        subformula2 =A6
+        form2    =A7
+        form3    =A8
+        form4    =A9
+        form5    =A10
+        form6    =A11
+        form7    =A12
+        form8    =A13
         +retrieval>
         isa     proposition
         thing   proposition
@@ -51,10 +67,10 @@ def module1(aBoxCon): #This module looks for concept assignments and negated con
         form    ~=A13
     """)
 
-    aBoxCon.productionstring(name="Module 1, Unit 2a: skip and derive new formula, Step 1", string="""
+    aBoxCon.productionstring(name="Module 1, Unit 2a, Step 1: find concept of negation not in the list", string="""
         =g>
         isa     goal
-        state   next_concept_negation
+        state   find_formula_not_in_list
         form    =P
         =imaginal>
         isa     checklist
@@ -77,7 +93,7 @@ def module1(aBoxCon): #This module looks for concept assignments and negated con
         ==>
         =g>
         isa      goal
-        state    find_clash
+        state    add_formula_to_list
         form     =P
         =imaginal>
         isa     checklist
@@ -113,10 +129,10 @@ def module1(aBoxCon): #This module looks for concept assignments and negated con
         form    ~=A13
     """)
 
-    aBoxCon.productionstring(name="Module 1, Unit 2a: skip and derive new formula, Step 1", string="""
+    aBoxCon.productionstring(name="Module 1, Unit 2b, Step 1: signal a clash", string="""
         =g>
         isa     goal
-        state   next_concept_negation
+        state   find_formula_not_in_list
         form    =P
         =imaginal>
         isa     checklist
@@ -158,10 +174,10 @@ def module1(aBoxCon): #This module looks for concept assignments and negated con
         key     C
     """)
 
-    aBoxCon.productionstring(name="Module 1, Unit 2a: skip and derive new formula, Step 1", string="""
+    aBoxCon.productionstring(name="Module 1, Unit 3a, Step 1: no formula found, need to derive new ones", string="""
         =g>
         isa     goal
-        state   find_clash
+        state   add_formula_to_list
         form    =P
         =imaginal>
         isa     checklist
@@ -184,8 +200,20 @@ def module1(aBoxCon): #This module looks for concept assignments and negated con
         ==>
         =g>
         isa      goal
-        state    derive_next
+        state    derive_next_formulas
         form     =P
+        ~imaginal>
+        =retrieval>
+        isa     storelist
+        thing   storelist
+        form    =P
+    """)
+
+    aBoxCon.productionstring(name="Module 1, Unit 3b, Step 1: add formula to list and find a clash", string="""
+        =g>
+        isa     goal
+        state   add_formula_to_list
+        form    =P
         =imaginal>
         isa     checklist
         thing   checklist
@@ -203,22 +231,42 @@ def module1(aBoxCon): #This module looks for concept assignments and negated con
         form7    =A12
         form8    =A13
         =retrieval>
-        isa     proposition
-        thing   proposition
-        derived yes
-        mainconnective ~disjunction
-        mainconnective ~conjunction
-        mainconnective ~existential
-        mainconnective ~universal
-        form    ~=A1
-        form    ~=A7
-        form    ~=A8
-        form    ~=A9
-        form    ~=A10
-        form    ~=A11
-        form    ~=A12
-        form    ~=A13
+        isa      proposition
+        thing    proposition
+        derived  yes
+        form     =X1
+        element  =X2
+        mainconnective =X3
+        relation =X4
+        subformula1 =X5
+        subformula2 =X6
+        ==>
+        =g>
+        isa      goal
+        state    find_clash_to_head
+        form     =P
+        =imaginal>
+        isa     checklist
+        thing   checklist
+        form    =X1
+        element =X2
+        mainconnective =X3
+        relation =X4
+        subformula1 =X5
+        subformula2 =X6
+        form2    =A1
+        form3    =A7
+        form4    =A8
+        form5    =A9
+        form6    =A10
+        form7    =A11
+        form8    =A12
+        ~retrieval>
     """)
+
+
+
+
 ----------------------------
     aBoxCon.productionstring(name="Module 1, Unit 2a: skip and derive new formula, Step 1", string="""
         =g>
@@ -1646,7 +1694,7 @@ def module3(aBoxCon): #This module decides if it's worth to look for a clash. If
     """)
 
 
-def module4(aBoxCon): #This module retrievs the list of all formulas that are already used. It then chooses a new formula not in the list. If there is no such formula, it confirms consistency.
+def module4(aBoxCon): #This module retrieves the list of all formulas that are already used. It then chooses a new formula not in the list. If there is no such formula, it confirms consistency.
     aBoxCon.productionstring(name="Module 4, Unit 1: check if there is a formula to store in the list of used formulas, Step 1", string="""
         =g>
         isa      goal
--- parser.py
+++ parser.py
@@ -4,18 +4,18 @@ import pyactr as actr
 
 form_grammar = """
 
-    ?start: assertion (","+ assertion)* 
+    ?start: assertion (","+ assertion)*
 
     ?assertion: element ":" out_concept             -> con_ass
         | "(" element "," element ")" ":" role  -> role_ass
 
-    ?out_concept: atom                            
+    ?out_concept: atom
         | "-" atom                          -> neg
         | ["("] concept "&" concept [")"]   -> conj
         | ["("] concept "%" concept [")"]   -> dis
         | "/E" role "." concept             -> exists
 
-    ?concept: atom                            
+    ?concept: atom
         | "-" atom                      -> neg
         | "(" concept "&" concept ")"   -> conj
         | "(" concept "%" concept ")"   -> dis
@@ -33,9 +33,9 @@ form_grammar = """
 
 @v_args(inline=True)    # Affects the signatures of the methods
 class ToString(Transformer): # Transforms a tree recursively to a string
-    role = str 
-    element = str 
-    atom = str 
+    role = str
+    element = str
+    atom = str
     conj = lambda self,ch1,ch2: "(" + ch1 + "&" + ch2 + ")"
     dis = lambda self,ch1,ch2: "(" + ch1 + "%" + ch2 + ")"
     exists = lambda self,role,con: "/E " + role + "." + con
@@ -43,7 +43,7 @@ class ToString(Transformer): # Transforms a tree recursively to a string
     role_ass = lambda self,elL,elR,role: "(" + elL + "," + elR + "):" + role
     neg = lambda self,con : "-" + con
 
-class AddFormToAbox(Visitor): # Adds a formula together with all subformulas to the DM. 
+class AddFormToAbox(Visitor): # Adds a formula together with all subformulas to the DM.
 
     def __init__(self,addtodm):
         self.derived="yes"
@@ -58,30 +58,30 @@ class AddFormToAbox(Visitor): # Adds a formula together with all subformulas to 
         elR = tree.children[1].children[0]
         role = ToString().transform(tree.children[2])
         formstr = ToString().transform(tree)
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition", 
-            form=formstr, element=elL, mainconnective="relation", 
+        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+            form=formstr, element=elL, mainconnective="relation",
             subformula1=elL, subformula2=elR, relation=role,
-            derived=self.derived, inferred1="no"))
+            derived=self.derived))
         self.derived="no"
 
     def conj(self,tree):
         formstr = self.el + ":" + ToString().transform(tree)
         subformL = self.el + ":" + ToString().transform(tree.children[0])
         subformR = self.el + ":" + ToString().transform(tree.children[1])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition", 
-            form=formstr, element=self.el, mainconnective="conjunction", 
-            subformula1=subformL, subformula2=subformR, 
-            derived=self.derived, inferred1="no", inferred2="no"))
+        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+            form=formstr, element=self.el, mainconnective="conjunction",
+            subformula1=subformL, subformula2=subformR,
+            derived=self.derived))
         self.derived="no"
 
     def dis(self,tree):
         formstr = self.el + ":" + ToString().transform(tree)
         subformL = self.el + ":" + ToString().transform(tree.children[0])
         subformR = self.el + ":" + ToString().transform(tree.children[1])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition", 
-            form=formstr, element=self.el, mainconnective="disjunction", 
-            subformula1=subformL, subformula2=subformR, 
-            derived=self.derived, inferred1="no", inferred2="no"))
+        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+            form=formstr, element=self.el, mainconnective="disjunction",
+            subformula1=subformL, subformula2=subformR,
+            derived=self.derived))
         self.derived="no"
 
     def exists(self,tree):
@@ -90,31 +90,31 @@ class AddFormToAbox(Visitor): # Adds a formula together with all subformulas to 
         formstr = self.el + ":" + ToString().transform(tree)
         subform = witness + ":" + ToString().transform(tree.children[1])
         role = ToString().transform(tree.children[0])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition", 
-            form=formstr, element=self.el, mainconnective="existential", 
-            subformula1=subform,  
-            derived=self.derived, inferred1="no", inferred2="no"))
+        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+            form=formstr, element=self.el, mainconnective="existential",
+            subformula1=subform, subformula2="("+self.el +","+witness+"):"+role,
+            derived=self.derived))
         self.derived="no"
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition", 
-            form="("+self.el +","+witness+"):"+role, relation=role, 
-            mainconnective="relation", subformula1=self.el, subformula2=witness, 
-            derived=self.derived, inferred1="no"))
+        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+            form="("+self.el +","+witness+"):"+role, relation=role,
+            mainconnective="relation", subformula1=self.el, subformula2=witness,
+            derived=self.derived))
         self.el = witness
 
     def neg(self,tree):
         formstr = self.el + ":" + ToString().transform(tree)
         subform = ToString().transform(tree.children[0])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition", 
-            form=formstr, element=self.el, mainconnective="negation", 
-            subformula1=subform, derived=self.derived, inferred1="no"))
+        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+            form=formstr, element=self.el, mainconnective="negation",
+            subformula1=subform, derived=self.derived))
         self.derived="no"
 
     def atom(self,tree):
         formstr = self.el + ":" + ToString().transform(tree)
         subform = ToString().transform(tree)
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition", 
-            form=formstr, element=self.el, mainconnective="concept", 
-            subformula1=subform, derived=self.derived, inferred1="no"))
+        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+            form=formstr, element=self.el, mainconnective="concept",
+            subformula1=subform, derived=self.derived))
         self.derived="no"
 
 form_parser = Lark(form_grammar, parser='lalr')
@@ -122,7 +122,7 @@ parser = form_parser.parse
 
 def AddAboxFromFile(filename,addtodm):
     with open(filename, 'r') as file:
-        data = file.read().replace('\n', ' ') 
+        data = file.read().replace('\n', ' ')
     abox = parser(data)
     if abox.data in ["con_ass","role_ass"]:
         aboxlst = [abox]
@@ -131,12 +131,11 @@ def AddAboxFromFile(filename,addtodm):
     for form in aboxlst:
         addform = AddFormToAbox(addtodm)
         addform.visit_topdown(form) # Need to visit the tree top down to get the element first.
-    
+
 if __name__ == "__main__":
     import pyactr as actr
     aBoxCon = actr.ACTRModel()
-    actr.chunktype("proposition", "thing, form, element, mainconnective, relation, subformula1, inferred1, subformula2, inferred2, derived")
+    actr.chunktype("proposition", "thing, form, element, mainconnective, relation, subformula1, subformula2, derived")
     dm = aBoxCon.decmem
     AddAboxFromFile("abox.txt",dm.add)
     print(dm)
-
