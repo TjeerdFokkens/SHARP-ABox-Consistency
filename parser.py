78972d03b64f289c91d8c05f586def2654a584b7
feffemannen <fredrik.ellborg@gmail.com>
2022-02-23 21:32:30 +0100

    Brute force addition of lots of constants that should cover the universal quantifiers. Had to remove subformula2 from propositions with mainconnective=existential. Hope that works.

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from lark import Lark, Transformer, Visitor, v_args
 import pyactr as actr
+import pprint
 #import pprint
 
 form_grammar = """
@@ -67,6 +68,7 @@ class AddFormToAbox(Visitor): # Adds a formula together with all subformulas to 
 
     def __init__(self,addtodm,elements):
         self.derived="yes"
+        self.insideuniversal=False
         self.witness=1
         self.addtodm=addtodm
         self.elements=elements
@@ -86,65 +88,107 @@ class AddFormToAbox(Visitor): # Adds a formula together with all subformulas to 
         self.derived="no"
 
     def conj(self,tree):
-        conceptstr = ToString().transform(tree)
-        formstr = self.el + ":" + conceptstr
-        subformL = self.el + ":" + ToString().transform(tree.children[0])
-        subformR = self.el + ":" + ToString().transform(tree.children[1])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
-            form=formstr, concept=conceptstr, element=self.el, mainconnective="conjunction",
-            subformula1=subformL, subformula2=subformR,
-            derived=self.derived))
+        constr = ToString().transform(tree)
+        subconL = ToString().transform(tree.children[0])
+        subconR = ToString().transform(tree.children[1])
+        def addconj(el):
+            self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+                    form=el + ":" + constr, concept=constr, element=el, mainconnective="conjunction",
+                    subformula1=el + ":" + subconL, subformula2=el + ":" + subconR,
+                    derived=self.derived))
+        if self.insideuniversal:
+            for el in self.elements:
+                addconj(el)
+        else:
+            addconj(self.el)
         self.derived="no"
 
     def dis(self,tree):
-        conceptstr = ToString().transform(tree)
-        formstr = self.el + ":" + conceptstr
-        subformL = self.el + ":" + ToString().transform(tree.children[0])
-        subformR = self.el + ":" + ToString().transform(tree.children[1])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
-            form=formstr, concept=conceptstr, element=self.el, mainconnective="disjunction",
-            subformula1=subformL, subformula2=subformR,
-            derived=self.derived))
+        constr = ToString().transform(tree)
+        subconL = ToString().transform(tree.children[0])
+        subconR = ToString().transform(tree.children[1])
+        def adddisj(el):
+            self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+                    form=el + ":" + constr, concept=constr, element=el, mainconnective="disjunction",
+                    subformula1=el + ":" + subconL, subformula2=el + ":" + subconR,
+                    derived=self.derived))
+        if self.insideuniversal:
+            for el in self.elements:
+                adddisj(el)
+        else:
+            adddisj(self.el)
         self.derived="no"
 
     def exists(self,tree):
-        conceptstr = ToString().transform(tree)
-        formstr = self.el + ":" + conceptstr
-        witness = "X" + str(self.witness)
-        self.witness=self.witness+1
-        subform = witness + ":" + ToString().transform(tree.children[1])
+        constr = ToString().transform(tree)
         role = ToString().transform(tree.children[0])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
-            form=formstr, concept=conceptstr, element=self.el, mainconnective="existential",
-            subformula1=subform, subformula2="("+self.el +","+witness+"):"+role,
-            derived=self.derived))
+        subcon = ToString().transform(tree.children[1])
+        def addrole(e1,e2):
+            self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+                form="(" + e1 +","+e2+"):"+role, relation=role,
+                mainconnective="relation", subformula1=e1, subformula2=e2,
+                derived="no"))
+        def addex(el):
+            self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+                    form=el + ":" + constr, concept=constr, element=el, mainconnective="existential",
+                    subformula1=el + ":" + subcon, derived=self.derived))
+        if self.insideuniversal:
+            for el in self.elements:
+                addex(el)
+                for w in self.elements:
+                    addrole(el,w)
+        else:
+            witness = "X" + str(self.witness)
+            self.witness=self.witness+1
+            addex(self.el)
+            addrole(self.el,witness)
+            self.el = witness
         self.derived="no"
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
-            form="("+self.el +","+witness+"):"+role, relation=role,
-            mainconnective="relation", subformula1=self.el, subformula2=witness,
-            derived=self.derived))
-        self.el = witness
+        
 
-    def universal(self,tree): #This function needs to be updated.
-        conceptstr = ToString().transform(tree)        
-        formstr = self.el + ":" + conceptstr
-        subform = self.el + ":" + ToString().transform(tree.children[1])
+    def universal(self,tree):
+        constr = ToString().transform(tree)
         role = ToString().transform(tree.children[0])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
-            form=formstr, concept=conceptstr, element=self.el, mainconnective="universal",
-            subformula1=subform, derived=self.derived))
+        subcon = ToString().transform(tree.children[1])
+        def addun(el):
+            self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+                    form=el + ":" + constr, concept=constr, element=el, mainconnective="universal",
+                    subformula1=el + ":" + subcon, derived=self.derived))
+        if self.insideuniversal:
+            for el in self.elements:
+                addun(el)
+        else:
+            addun(self.el)
         self.derived="no"
+        self.insideuniversal = True
 
     def neg(self,tree):
-        conceptstr = ToString().transform(tree)        
-        formstr = self.el + ":" + conceptstr
-        subform = ToString().transform(tree.children[0])
-        self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
-            form=formstr, concept=conceptstr, element=self.el, mainconnective="negation",
-            subformula1=subform, derived=self.derived))
+        constr = ToString().transform(tree)
+        subcon = ToString().transform(tree.children[0])
+        def addneg(el):
+            self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+                    form=el + ":" + constr, concept=constr, element=el, mainconnective="negation",
+                    subformula1=subcon, derived=self.derived))
+        if self.insideuniversal:
+            for el in self.elements:
+                addneg(el)
+        else:
+            addneg(self.el)
         self.derived="no"
 
     def atom(self,tree):
+        constr = ToString().transform(tree)
+        def addatom(el):
+            self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
+                    form=el + ":" + constr, concept=constr, element=el, mainconnective="concept",
+                    subformula1=constr, derived=self.derived))
+        if self.insideuniversal:
+            for el in self.elements:
+                addatom(el)
+        else:
+            addatom(self.el)
+        self.derived="no"
+
         formstr = self.el + ":" + ToString().transform(tree)
         subform = ToString().transform(tree)
         self.addtodm(actr.makechunk(typename="proposition", thing="proposition",
@@ -179,4 +223,7 @@ if __name__ == "__main__":
     actr.chunktype("proposition", "thing, concept, form, element, mainconnective, relation, subformula1, subformula2, derived")
     dm = aBoxCon.decmem
     AddAboxFromFile("abox.txt",dm.add)
-    #print(dm)
+    for a in dm:
+        print (str(a[3][1]) + " :: " + str(a))
+    
+
