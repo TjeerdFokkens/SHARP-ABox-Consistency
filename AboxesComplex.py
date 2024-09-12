1ff9626e60109361900e8f3369545316593333ab
TjeerdFokkens <tjeerdfokkens@gmail.com>
2024-09-12 17:00:28 +0200

    Delete AboxesComplex.py

--- AboxesComplex.py
+++
@@ -1,1 +0,0 @@
-aboxes_complex = ['a:/Er./Ar./Ar./Er./Ar./Ar.T, a:/Ar./Er./Ar./Ar./Er./Ar.T, a:/Er./Ar./Ar./Er./Ar./Ar.T, a:/Ar./Er./Er./Ar./Er./Er.T, a:/Er./Ar./Er./Er./Ar./Er.T, a:/Ar./Ar./Ar./Ar./Ar./Ar.-T']
